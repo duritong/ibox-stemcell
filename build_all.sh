5f3d7c3f47d12cb7bd3738f6c6a36e92e11f297a
mh <mh@immerda.ch>
2015-02-09 21:28:51 +0100

    add a build wrapper

---
+++ build_all.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+providers="qemu virtualbox"
+templates="centos7"
+which packer.io 2>&1 > /dev/null
+if [ $? -gt 0 ]; then
+  alias packer.io='packer'
+fi
+
+for provider in $providers; do
+  if [ $provider = 'virtualbox' ]; then
+    only=virtualbox-iso
+  else
+    only=$provider
+  fi
+  for template in $templates; do
+    echo "Building ${template}-${provider}"
+    packer.io build -only=${only} ${template}.json
+    echo "(Re)packaging ${template}-${provider}"
+    if [ $provider = 'qemu' ]; then
+      scripts/qemu_to_box.sh ibox-${template}-x86_64-qemu/packer-ibox-${template}-x86_64-qemu.qcow2
+    else
+      # hack the version into the vagrant file
+      scripts/vagrant_set_version.sh builds/$provider/ibox-${template}-x86_64-${provider}.box
+    fi
+  done
+done
